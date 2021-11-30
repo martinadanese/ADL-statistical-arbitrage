d14e540288afbe57db7ffd2eb82eb36d360034ae
martinadanese <martinadanese.md@gmail.com>
2021-11-30 17:48:27 +0100

    created data loader module

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 from data_loader import DataLoader
 
-loader = DataLoader(reload_sp500=False)
-df_open, df_close = loader.get_data()
-print(df_open.tail())
+loader = DataLoader(reload_sp500=False, number_of_data=10)
+df_open, df_close = loader.get_data(verbose=True)
+
+print(df_open.head())
